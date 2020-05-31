bbdc3d7a253c792fc5b3f1abcc22d358e816fadc
Niklas <niklas.linz@enigmar.de>
2020-05-31 21:27:06 +0200

    add solar status

--- src/main/java/de/linzn/homeWebApp/api/jqueryContent/DivServer.java
+++ src/main/java/de/linzn/homeWebApp/api/jqueryContent/DivServer.java
@@ -16,6 +16,7 @@ import de.linzn.homeWebApp.core.htmlTemplates.EmptyTemplate;
 import de.linzn.homeWebApp.core.htmlTemplates.IHtmlTemplate;
 import de.linzn.serviceStatus.callbacks.PlexServerStatus;
 import de.linzn.serviceStatus.callbacks.ProxmoxStatus;
+import de.linzn.serviceStatus.callbacks.SolarStatus;
 import de.linzn.systemChain.callbacks.TemperatureScheduler;
 
 import java.util.List;
@@ -27,9 +28,15 @@ public class DivServer implements IResponseHandler {
 
         StringBuilder stringBuilder = new StringBuilder();
 
+        boolean solarBerry = SolarStatus.getSolarStatus();
         boolean plexServer = PlexServerStatus.getPlexStatus();
         boolean proxmox = ProxmoxStatus.getProxmoxStatus();
         double serverTemp = TemperatureScheduler.getHottestCore();
+
+        String solarStatus = "<bad>OFFLINE</bad>";
+        if (solarBerry) {
+            solarStatus = "<ok>ONLINE </ok>";
+        }
 
         String plexStatus = "<bad>OFFLINE</bad>";
         if (plexServer) {
@@ -48,6 +55,7 @@ public class DivServer implements IResponseHandler {
 
         stringBuilder.append("<div id=\"server\">" +
                 "    <div class=\"cont\">" +
+                "        <h2 style=\"font-size: 22px;font-weight: normal;\">SOLAR | " + solarStatus + "</h2>" +
                 "        <h2 style=\"font-size: 22px;font-weight: normal;\">PLEX | " + plexStatus + "</h2>" +
                 "        <h2 style=\"font-size: 22px;font-weight: normal;\">PROX | " + proxmoxStatus + "</h2>" +
                 "        <h2 style=\"font-size: 22px;font-weight: normal;\">CORE | " + tempStatus + "</h2>" +
